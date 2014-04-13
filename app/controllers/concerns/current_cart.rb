13e04d83411d17a3337d302b8bdb66678a78c267
ImanKhadiv <iman.khadiv@gmail.com>
2014-04-13 13:12:47 +0100

    added playtime p117

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,9 +4,17 @@ module CurrentCart
 	private
 
 	def set_cart
+		session[:counter] = nil
 		@cart = Cart.find(session[:cart_id])
 	rescue  ActiveRecord::RecordNotFound
 		@cart = Cart.create
 		session[:cart_id] = @cart.id
 	end
+
+	def increment_count
+		session[:counter] ||= 0
+		session[:counter] += 1
+	end
+
+		
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+	  include CurrentCart
+	  before_action :increment_count, only: [:index] 
+
   def index
   	@products = Product.order(:title)
+
+  	 # @count = increment_count
   end
 end
