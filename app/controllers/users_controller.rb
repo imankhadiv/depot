a13498096a07e7043b509cec59b6b616ab5c665f
ImanKhadiv <iman.khadiv@gmail.com>
2014-06-03 09:46:30 +0100

    added layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,13 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    begin
     @user.destroy
+      flash[:notice] = "user #{@user.name} deleted"
+    rescue StandardError => e
+      # flash[:notice] = e.message
+      flash[:notice] = e.message
+    end
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
